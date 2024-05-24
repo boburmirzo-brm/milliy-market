242a2671b3ab27fc4107f23b3ed898a787bdacce
Saidaziz007 <saidaminovvs@gmail.com>
2024-05-24 15:03:53 +0500

    commit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,13 @@
-import React, { memo } from 'react'
-import ProductWrapper from '../../components/product/ProductWrapper'
-import { PRODUCTS } from "../../static"
+import React, { memo } from "react";
+import ProductWrapper from "../../components/product/ProductWrapper";
+import { PRODUCTS } from "../../static";
 
 const Home = () => {
-
   return (
     <div>
-      <h2>Home</h2>
       <ProductWrapper data={PRODUCTS} />
     </div>
-  )
-}
+  );
+};
 
-export default memo(Home)
+export default memo(Home);
