56ed3832c7a8ae6db82c4ec58779f1e50e9c9b2f
Nomonjonprog <ibragimovganijon354@gmail.com>
2024-05-26 10:53:12 +0300

    about

--- src/components/statistika/Static.jsx
+++ src/components/statistika/Static.jsx
@@ -1,30 +1,29 @@
 import React from "react";
-import icon1 from "../../assets/images/about/icon1.png";
-import icon2 from "../../assets/images/about/icon2.png";
-import icon3 from "../../assets/images/about/icon3.png";
-import icon4 from "../../assets/images/about/icon4.png";
+import { AiOutlineShop, AiOutlineDollar } from "react-icons/ai";
+import { FiShoppingBag } from "react-icons/fi";
+import { FaSackDollar } from "react-icons/fa6";
 
 const Static = () => {
   return (
-    <div className="container static">
-      <div className="st__card">
-        <img src={icon1} alt="" />
-        <p className="title">10.5k</p>
+    <div className="container statistika">
+      <div className="statistika__card">
+        <AiOutlineShop className="statistika__icon" />
+        <p className="statistika__title">10.5k</p>
         <p>Sotuvchilar saytimizni faollashtirmoqda</p>
       </div>
-      <div className="st__card st__card2">
-        <img src={icon2} alt="" />
-        <p className="title">33k</p>
+      <div className="statistika__card">
+        <AiOutlineDollar className="statistika__icon" />
+        <p className="statistika__title">33k</p>
         <p>Oylik mahsulot savdosi</p>
       </div>
-      <div className="st__card">
-        <img src={icon3} alt="" />
-        <p className="title">45.5k</p>
+      <div className="statistika__card">
+        <FiShoppingBag className="statistika__icon" />
+        <p className="statistika__title">45.5k</p>
         <p>Saytimizda faol mijoz</p>
       </div>
-      <div className="st__card">
-        <img src={icon4} alt="" />
-        <p className="title">25k</p>
+      <div className="statistika__card">
+        <FaSackDollar className="statistika__icon" />
+        <p className="statistika__title">25k</p>
         <p>Bizning saytimizda yillik yalpi savdo</p>
       </div>
     </div>
