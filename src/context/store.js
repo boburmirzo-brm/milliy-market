cd3a86d33f896303c868cfa76dd48f7db58f4c9c
Nomonjonprog <ibragimovganijon354@gmail.com>
2024-05-28 21:58:12 +0300

    cart

--- src/context/store.js
+++ src/context/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api/index";
+import cartSlice from "./slice/cartSlice";
 
 export const store = configureStore({
-    reducer: {
-        [api.reducerPath]: api.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(api.middleware)
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    cart: cartSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
