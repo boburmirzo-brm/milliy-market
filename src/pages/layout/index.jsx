5718e769b904ef9bf631356061a7abe306bc77b0
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-29 11:40:26 +0500

    yahyo breadcrumb

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,18 +1,19 @@
-import React, { memo } from 'react'
-import Header from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
-import { Outlet } from 'react-router-dom'
-import SubHeader from '../../components/sub-header/SubHeader'
-
+import React, { memo } from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { Outlet } from "react-router-dom";
+import SubHeader from "../../components/sub-header/SubHeader";
+import BreadCrumbs from "../../components/breadcrumbs/BreadCrumbs";
 const Layout = () => {
   return (
     <>
       <SubHeader />
       <Header />
+      <BreadCrumbs />
       <Outlet />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default memo(Layout)
+export default memo(Layout);
